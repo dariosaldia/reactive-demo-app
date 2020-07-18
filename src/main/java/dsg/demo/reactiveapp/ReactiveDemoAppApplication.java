249e78f1bb4bc590257f4b61b6c9e7bb8f71057d
Dario <dariosaldia@gmail.com>
2020-07-18 16:39:48 -0300

    Initial commit

---
+++ src/main/java/dsg/demo/reactiveapp/ReactiveDemoAppApplication.java
@@ -0,0 +1,13 @@
+package dsg.demo.reactiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsg/demo/reactiveapp/ReactiveDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dsg.demo.reactiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
